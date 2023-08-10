b7322ad96a6b0dd47d21cbd48030b5ec92bd879e
Audrey <sonzy2@gmail.com>
2023-08-10 16:34:29 +0200

    implementation d ela navigation

--- src/main/java/fr/eni/filmotheque/ihm/FilmController.java
+++ src/main/java/fr/eni/filmotheque/ihm/FilmController.java
@@ -1,8 +1,41 @@
 package fr.eni.filmotheque.ihm;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FilmController {
+	
+	@GetMapping({"/", "/accueil"})
 
+    public String afficherAccueil() {
+
+        return "listeFilm";
+
+    }   
+   
+    @GetMapping("/details")
+
+    public String afficherdetails() {
+
+        return "detailsFilm";
+
+    }
+       
+
+    @GetMapping("/ajout")
+
+    public String afficherFormulaireFilm() {
+
+        return "ajoutFilm";
+
+    }
+    
+    @PostMapping("/ajout")
+    public String ajouterFilm() {
+        return "redirect:/accueil";
+
+    }
+	
 }
